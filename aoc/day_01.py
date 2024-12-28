bd91fae8217fdff6743fa7e747741b103e9dc459
Joe Boyle <jboyle617@gmail.com>
2024-12-28 00:03:13 -0500

    simplify

--- aoc/day_01.py
+++ aoc/day_01.py
@@ -3,28 +3,19 @@ import os
 
 
 def part_1_get_sum(left: list[int], right: list[int]) -> int:
-    total = 0
-    for i, left_item in enumerate(left):
-        diff = abs(left_item - right[i])
-        total += diff
-    return total
+    return sum(abs(left_val - right[i]) for (i, left_val) in enumerate(left))
 
 
 def part_2_get_similarity(left: list[int], right: list[int]):
-    total = 0
-    right_counts = _get_counts(right)
-    for left_val in left:
-        print(left_val)
-        total += left_val * right_counts.get(left_val, 0)
-    return total
+    return sum(
+        left_val * _get_counts(right).get(left_val, 0) for left_val in left
+    )
 
 
 def _get_counts(inp: list[int]) -> dict[int, int]:
     out: dict[int, int] = {}
     for num in inp:
-        curr_count = out.get(num, 0)
-        out[num] = curr_count + 1
-    print(out)
+        out[num] = out.get(num, 0) + 1
     return out
 
 
@@ -38,8 +29,7 @@ def _split_lists(lists: str) -> tuple[list[int], list[int]]:
     return (sorted(left), sorted(right))
 
 
-def run():
-    filepath = os.path.join(os.getcwd(), "aoc", "day_01.txt")
+def run(filepath: str):
     with open(filepath, encoding="utf8") as file:
         left, right = _split_lists(file.read())
         print(part_2_get_similarity(left, right))
