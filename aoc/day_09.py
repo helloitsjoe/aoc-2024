f6a74f4abbf5d396f204eb0873bf9d39614e05e1
Joe Boyle <jboyle617@gmail.com>
2025-01-19 07:44:08 -0500

    day 9 part 1

--- aoc/day_09.py
+++ aoc/day_09.py
@@ -3,55 +3,41 @@ DATA_FILE = "day_09.txt"
 TEST_DATA = "2333133121414131402"
 
 
-def to_blocks(data: str) -> str:
+def to_blocks(data: str) -> list[int]:
     blocks = []
     block_id = 0
     for i, block in enumerate(data):
         if i == 0 or i % 2 == 0:
             for _ in range(int(block)):
-                blocks.append(str(block_id))
+                blocks.append(block_id)
             block_id += 1
         else:
             for _ in range(int(block)):
-                blocks.append(".")
-    result = "".join(blocks)
-    # print(result)
-    return result
+                blocks.append(-1)
+
+    # print(blocks)
+    return blocks
 
 
-def compact_blocks(data: str) -> str:
-    start_idx = 0
-    end_idx = len(data) - 1
-    compacted: list[str] = []
-    num_ints = len(data) - data.count(".")
+def compact_blocks(data: list[int]) -> list[int]:
+    data_list = data[:]
+    num_ints = len(data) - data.count(-1)
+    i = data_list.index(-1)
 
-    while end_idx >= start_idx:
-        print(end_idx, start_idx)
+    while len(data_list) > num_ints:
+        # Find the next empty space
+        while data_list[i] != -1:
+            i += 1
+        last = data_list.pop()
+        if last != -1:
+            data_list[i] = last
 
-        while data[end_idx] == ".":
-            end_idx -= 1
-
-        while data[start_idx] != ".":
-            if len(compacted) == num_ints:
-                break
-            compacted.append(data[start_idx])
-            start_idx += 1
-
-        if len(compacted) == num_ints:
-            break
-
-        compacted.append(data[end_idx])
-        start_idx += 1
-        end_idx -= 1
-
-    result = "".join(compacted)
-    # print(result)
-    return result
+    # print(data_list)
+    return data_list
 
 
-def get_checksum(data: str) -> int:
-    blocks = list(data.replace(".", ""))
-    return sum((i * int(block_id) for i, block_id in enumerate(blocks)))
+def get_checksum(blocks: list[int]) -> int:
+    return sum((i * block_id for i, block_id in enumerate(blocks)))
 
 
 def run(data: str):
--- aoc/tests/day_09_test.py
+++ aoc/tests/day_09_test.py
@@ -1,35 +1,99 @@
-from aoc.day_09 import to_blocks, compact_blocks, get_checksum, TEST_DATA
+from aoc.day_09 import run, to_blocks, compact_blocks, get_checksum, TEST_DATA
+
+
+def _to_ints(data: str) -> list[int]:
+    return list(map(_replace_dot, data))
+
+
+def _replace_dot(ea: str):
+    return int(ea.replace(".", "-1"))
 
 
 def test_to_blocks_basic():
     data = "12345"
-    assert to_blocks(data) == "0..111....22222"
+    assert to_blocks(data) == _to_ints("0..111....22222")
 
 
 def test_compact_blocks_basic():
-    data = "0..111....22222"
-    assert compact_blocks(data) == "022111222"
+    data = _to_ints("0..111....22222")
+    assert compact_blocks(data) == _to_ints("022111222")
 
 
 def test_checksum():
-    data = "022111222"
-    assert get_checksum(data) == 60
-
-
-def test_checksum_extra_dots():
-    data = "022111222....."
+    data = _to_ints("022111222")
     assert get_checksum(data) == 60
 
 
 def test_to_blocks_test_data():
-    assert to_blocks(TEST_DATA) == "00...111...2...333.44.5555.6666.777.888899"
+    assert to_blocks(TEST_DATA) == _to_ints(
+        "00...111...2...333.44.5555.6666.777.888899"
+    )
 
 
 def test_compact_blocks_test_data():
-    data = "00...111...2...333.44.5555.6666.777.888899"
-    assert compact_blocks(data) == "0099811188827773336446555566"
+    data = _to_ints("00...111...2...333.44.5555.6666.777.888899")
+    assert compact_blocks(data) == _to_ints("0099811188827773336446555566")
 
 
 def test_checksum_test_data():
-    data = "0099811188827773336446555566.............."
+    data = _to_ints("0099811188827773336446555566")
     assert get_checksum(data) == 1928
+
+
+def test_double_digit_id():
+    data = "233313312141413140243"
+    assert to_blocks(data) == [
+        0,
+        0,
+        -1,
+        -1,
+        -1,
+        1,
+        1,
+        1,
+        -1,
+        -1,
+        -1,
+        2,
+        -1,
+        -1,
+        -1,
+        3,
+        3,
+        3,
+        -1,
+        4,
+        4,
+        -1,
+        5,
+        5,
+        5,
+        5,
+        -1,
+        6,
+        6,
+        6,
+        6,
+        -1,
+        7,
+        7,
+        7,
+        -1,
+        8,
+        8,
+        8,
+        8,
+        9,
+        9,
+        -1,
+        -1,
+        -1,
+        -1,
+        10,
+        10,
+        10,
+    ]
+
+
+def test_run():
+    assert run(TEST_DATA) == 1928
