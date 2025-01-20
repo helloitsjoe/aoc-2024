4d2a20eb39ef681abc975a902c351a7d0d398810
Joe Boyle <jboyle617@gmail.com>
2025-01-19 21:05:22 -0500

    day 9 part 2 wip

--- aoc/day_09.py
+++ aoc/day_09.py
@@ -1,6 +1,11 @@
+import os
+from collections import namedtuple
+
 DATA_FILE = "day_09.txt"
 
 TEST_DATA = "2333133121414131402"
+
+FileBlock = namedtuple("FileBlock", ["idx", "len", "id"])
 
 
 def to_blocks(data: str) -> list[int]:
@@ -36,9 +41,125 @@ def compact_blocks(data: list[int]) -> list[int]:
     return data_list
 
 
+# TODO: make this clearer
+def create_slot_map(data_list: list[int]) -> list[tuple[int, int]]:
+    slot_map = []
+    start_idx = -1
+    cap = 0
+
+    for i, block in enumerate(data_list):
+        if block == -1:
+            start_idx = i if start_idx == -1 else start_idx
+            cap += 1
+        else:
+            if start_idx != -1:
+                slot_map.append((start_idx, cap))
+            start_idx = -1
+            cap = 0
+
+    if start_idx != -1:
+        slot_map.append((start_idx, cap))
+
+    return slot_map
+
+
+def create_block_map(data_list: list[int]) -> list[FileBlock]:
+    block_map = []
+    start_idx = -1
+    seen_block = -1
+    cap = 0
+
+    for i, block in enumerate(data_list):
+        if block != -1:
+            start_idx = i if start_idx == -1 else start_idx
+            seen_block = block
+            print(seen_block)
+            cap += 1
+        else:
+            if start_idx != -1:
+                block_map.append(FileBlock(start_idx, cap, seen_block))
+            start_idx = -1
+            cap = 0
+            seen_block = -1
+
+    # Add any blocks at the end
+    if start_idx != -1 and seen_block != -1:
+        block_map.append(FileBlock(start_idx, cap, seen_block))
+
+    return block_map
+
+
+def move_file_blocks(
+    data_list: list[int],
+    slot_map: list[tuple[int, int]],
+    block_map: list[FileBlock],
+) -> list[int]:
+    # Iterate once over data_list, trying to fit each into first empty slot
+
+    # Iterate backwards through data_list, looking only at full blocks
+    for block in block_map[::-1]:
+        print("block", block)
+        for i, ea in enumerate(slot_map):
+            idx, cap = ea
+            print(block.len, cap)
+            if block.len == cap:
+                # fill slot and update idx, cap, and remove from data_list
+                print("slot_map before", slot_map)
+                slot_map = slot_map[0:i] + slot_map[i + 1 :]
+                print("slot_map after", slot_map)
+
+                print("data_list before", data_list)
+                data_list = (
+                    data_list[:idx]
+                    + [block.id for _ in range(block.len)]
+                    + data_list[i + block.len + 1 : block.idx]
+                    + [-1 for _ in range(block.len)]
+                    + data_list[block.idx + block.len :]
+                )
+                print("data_list after", data_list)
+            elif block.len < cap:
+                # remove len from cap
+                # print("slot_map before", slot_map)
+                # slot_map[i] = (i, cap - block.len)
+                # print("slot_map after", slot_map)
+
+                # # add block to beginning, remove from end
+                # print("data_list before", data_list)
+                # data_list = (
+                #     data_list[:idx]
+                #     + [block.id for _ in range(block.len)]
+                #     + data_list[i + block.len + 1 : block.idx]
+                #     + [-1 for _ in range(block.len)]
+                #     + data_list[block.idx + block.len :]
+                # )
+                # print("data_list after", data_list)
+                break
+
+    return data_list
+
+
+def compact_file_blocks(data: list[int], data_map: str) -> list[int]:
+    data_list = data[:]
+
+    # Map of empty slots (index of data_list, capacity)
+    slot_map = create_slot_map(data_list)
+    block_map = create_block_map(data_list)
+    return move_file_blocks(
+        data_list,
+        slot_map,
+        block_map,
+    )
+
+
 def get_checksum(blocks: list[int]) -> int:
-    return sum((i * block_id for i, block_id in enumerate(blocks)))
+    return sum(
+        (i * block_id for i, block_id in enumerate(blocks) if block_id != -1)
+    )
 
 
 def run(data: str):
+    data_map = data.strip()
+    part_2 = os.getenv("PART_2") == "true"
+    if part_2:
+        return get_checksum(compact_file_blocks(to_blocks(data_map), data_map))
     return get_checksum(compact_blocks(to_blocks(data.strip())))
--- aoc/tests/day_09_test.py
+++ aoc/tests/day_09_test.py
@@ -1,4 +1,15 @@
-from aoc.day_09 import run, to_blocks, compact_blocks, get_checksum, TEST_DATA
+from aoc.day_09 import (
+    run,
+    to_blocks,
+    compact_blocks,
+    compact_file_blocks,
+    move_file_blocks,
+    create_block_map,
+    create_slot_map,
+    get_checksum,
+    FileBlock,
+    TEST_DATA,
+)
 
 
 def _to_ints(data: str) -> list[int]:
@@ -14,20 +25,15 @@ def test_to_blocks_basic():
     assert to_blocks(data) == _to_ints("0..111....22222")
 
 
-def test_compact_blocks_basic():
-    data = _to_ints("0..111....22222")
-    assert compact_blocks(data) == _to_ints("022111222")
-
-
-def test_checksum():
-    data = _to_ints("022111222")
-    assert get_checksum(data) == 60
-
-
 def test_to_blocks_test_data():
     assert to_blocks(TEST_DATA) == _to_ints(
         "00...111...2...333.44.5555.6666.777.888899"
     )
+
+
+def test_compact_blocks_basic():
+    data = _to_ints("0..111....22222")
+    assert compact_blocks(data) == _to_ints("022111222")
 
 
 def test_compact_blocks_test_data():
@@ -35,9 +41,84 @@ def test_compact_blocks_test_data():
     assert compact_blocks(data) == _to_ints("0099811188827773336446555566")
 
 
+def test_slot_map_basic():
+    orig_data = "1112321"
+    data = to_blocks(orig_data)
+    assert data == [0, -1, 1, -1, -1, 2, 2, 2, -1, -1, 3]
+    assert create_slot_map(data) == [(1, 1), (3, 2), (8, 2)]
+
+
+def test_block_map_basic():
+    orig_data = "1112321"
+    data = to_blocks(orig_data)
+    assert data == [0, -1, 1, -1, -1, 2, 2, 2, -1, -1, 3]
+    assert create_block_map(data) == [
+        FileBlock(idx=0, len=1, id=0),
+        FileBlock(idx=2, len=1, id=1),
+        FileBlock(idx=5, len=3, id=2),
+        FileBlock(idx=10, len=1, id=3),
+    ]
+
+
+def test_move_file_blocks():
+    orig_data = "1112321"
+    data = to_blocks(orig_data)
+    assert data == [0, -1, 1, -1, -1, 2, 2, 2, -1, -1, 3]
+    slot_map = create_slot_map(data)
+    block_map = create_block_map(data)
+    assert move_file_blocks(data, slot_map, block_map) == _to_ints(
+        "031..222..."
+    )
+
+
+def test_move_file_blocks_partial():
+    orig_data = "1312321"
+    data = to_blocks(orig_data)
+    assert data == [0, -1, -1, -1, 1, -1, -1, 2, 2, 2, -1, -1, 3]
+    slot_map = create_slot_map(data)
+    block_map = create_block_map(data)
+    assert move_file_blocks(data, slot_map, block_map) == _to_ints(
+        "03..1..222..."
+    )
+
+
+# def test_move_file_blocks_2():
+#     orig_data = "111232133"
+#     data = to_blocks(orig_data)
+#     assert data == [0, -1, 1, -1, -1, 2, 2, 2, -1, -1, 3, -1, -1, -1, 4, 4, 4]
+#     slot_map = create_slot_map(data)
+#     block_map = create_block_map(data)
+#     assert move_file_blocks(data, slot_map, block_map) == _to_ints(
+#         "031..222...444..."
+#     )
+
+
+# def test_compact_file_blocks_basic():
+#     orig_data = "1112321"
+#     data = to_blocks(orig_data)
+#     assert data == [0, -1, 1, -1, -1, 2, 2, 2, -1, -1, 3]
+#     assert compact_file_blocks(data, orig_data) == _to_ints("031..222..")
+
+
+# def test_compact_file_blocks_test():
+#     assert compact_file_blocks(_to_ints(TEST_DATA), TEST_DATA) == _to_ints(
+#         "00992111777.44.333....5555.6666.....8888.."
+#     )
+
+
+def test_checksum():
+    data = _to_ints("022111222")
+    assert get_checksum(data) == 60
+
+
 def test_checksum_test_data():
     data = _to_ints("0099811188827773336446555566")
     assert get_checksum(data) == 1928
+
+
+def test_checksum_test_data_ignore_empty():
+    data = [1, 0, -1, 3]
+    assert get_checksum(data) == 9
 
 
 def test_double_digit_id():
