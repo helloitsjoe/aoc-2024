ef1a477638c8813d2b34320132a4eab0ea122bc0
Joe Boyle <jboyle617@gmail.com>
2025-01-25 17:18:59 -0500

    day 12 part 1 wip

--- aoc/day_12.py
+++ aoc/day_12.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 DATA_FILE = "day_12.txt"
 
 TEST_DATA = """
@@ -21,50 +23,121 @@ EEEC
 #     return area, perimeter
 
 
-def convert_to_list(data: str):
-    return [list(line) for line in data.strip().splitlines()]
+DIRS = {
+    "N": (0, -1),
+    "E": (1, 0),
+    "S": (0, 1),
+    "W": (-1, 0),
+}
 
 
-def get_areas(land: list[list[str]]):
-    areas: dict[str, int] = {}
+@dataclass
+class Square:
+    val: str
+    visited: bool = False
 
-    for row in land:
-        for letter in row:
-            areas[letter] = areas.get(letter, 0) + 1
-
-    return areas
+    def __repr__(self):
+        return f"{self.val} | {self.visited}"
 
 
-def get_perimeters(land: list[list[str]]):
-    perimeters: dict[str, int] = {}
+type Land = list[list[Square]]
+type Plot = tuple[str, int, int]
+
+
+def create_land(data: str):
+    return [list(map(Square, line)) for line in data.strip().splitlines()]
+
+
+def spread_from(
+    x: int, y: int, land: list[list[Square]]
+) -> tuple[str, int, int]:
+    area = 0
+    perimeter = 0
+    curr = land[y][x]
+    curr_x = x
+    curr_y = y
+    curr_letter = curr.val
+    to_check = [(curr, x, y)]
+
+    # TODO: Move to each next square... How to determine when there are no more
+    # squares in the current plot?
+    while to_check:
+        for dir_x, dir_y in DIRS.values():
+            next_x = curr_x + dir_x
+            next_y = curr_y + dir_y
+
+            if 0 <= next_y < len(land) and 0 <= next_x < len(land[y]):
+                next_sq = land[next_y][next_x]
+                if next_sq.visited or next_sq.val != curr.val:
+                    continue
+
+                print("appending", next_sq, next_x, next_y)
+                to_check.append((next_sq, next_x, next_y))
+
+                # Add to area
+                area += 1
+
+                # Add to perimeter
+                #     Check each side
+                #     Subtract 2 if same letter (should skip if visited?)
+                # perimeter += 4
+
+        # Mark as visited
+        curr.visited = True
+        # Move on
+        (curr, curr_x, curr_y) = to_check.pop()
+
+    print(area, perimeter)
+    return curr_letter, area, perimeter
+
+
+def get_plots(land: Land):
+    plots: list[Plot] = []
 
     for y, row in enumerate(land):
-        for x, letter in enumerate(row):
-            perimeters[letter] = perimeters.get(letter, 0) + 4
-            if 0 <= (x - 1) < len(land[y]) and land[y][x - 1] == letter:
-                perimeters[letter] -= 2
-            if 0 <= (y - 1) < len(land) and land[y - 1][x] == letter:
-                perimeters[letter] -= 2
+        for x, sq in enumerate(row):
+            print(row)
+            if sq.visited:
+                continue
+            letter, area, perimeter = spread_from(x, y, land)
+            plots.append((letter, area, perimeter))
 
-    return perimeters
+    return plots
+
+
+# def get_perimeters(land: list[list[Square]]):
+#     perimeters: dict[str, int] = {}
+
+#     for y, row in enumerate(land):
+#         for x, letter in enumerate(row):
+#             perimeters[letter] = perimeters.get(letter, 0) + 4
+#             if 0 <= (x - 1) < len(land[y]) and land[y][x - 1] == letter:
+#                 perimeters[letter] -= 2
+#             if 0 <= (y - 1) < len(land) and land[y - 1][x] == letter:
+#                 perimeters[letter] -= 2
+
+#     return perimeters
 
 
 def combine_area_and_perimeter(
-    areas: dict[str, int], perimeters: dict[str, int]
+    # areas: dict[str, int], perimeters: dict[str, int]
+    plots: list[Plot],
 ):
     total = 0
-    for letter in areas:
-        total += areas[letter] * perimeters[letter]
+    for letter, area, perimeter in plots:
+        total += area * perimeter
 
     return total
 
 
 def run(data: str, __part_2: bool = False):
-    land = convert_to_list(data)
+    land = create_land(data)
 
-    areas = get_areas(land)
-    perimeters = get_perimeters(land)
-    print(areas)
-    print(perimeters)
+    # areas = get_areas(land)
+    # perimeters = get_perimeters(land)
+    plots = get_plots(land)
+    # print(areas)
+    # print(perimeters)
+    print(plots)
 
-    return combine_area_and_perimeter(areas, perimeters)
+    return combine_area_and_perimeter(plots)
--- aoc/tests/day_12_test.py
+++ aoc/tests/day_12_test.py
@@ -1,4 +1,4 @@
-from aoc.day_12 import run, get_areas, get_perimeters, convert_to_list
+from aoc.day_12 import run, get_plots, create_land
 
 TEST_DATA_SMALL = """
 AAAA
@@ -15,36 +15,46 @@ OXOXO
 OOOOO
 """
 
-test_data_small_list = convert_to_list(TEST_DATA_SMALL)
-test_data_med_list = convert_to_list(TEST_DATA_MED)
+test_data_small_list = create_land(TEST_DATA_SMALL)
+test_data_med_list = create_land(TEST_DATA_MED)
 
 
-def test_get_areas_small():
-    areas = {"A": 4, "B": 4, "C": 4, "D": 1, "E": 3}
-    assert get_areas(test_data_small_list) == areas
+def test_get_plots_small():
+    assert sorted(get_plots(test_data_small_list)) == [
+        ("A", 4, 10),
+        ("B", 4, 8),
+        ("C", 4, 10),
+        ("D", 1, 4),
+        ("E", 3, 8),
+    ]
 
 
-def test_get_areas_med():
-    areas = {"O": 21, "X": 4}
-    assert get_areas(test_data_med_list) == areas
+# def test_get_areas_small():
+#     areas = {"A": 4, "B": 4, "C": 4, "D": 1, "E": 3}
+#     assert get_areas(test_data_small_list) == areas
 
 
-def test_get_perimeters_small():
-    perimeters = {"A": 10, "B": 8, "C": 10, "D": 4, "E": 8}
-    assert get_perimeters(test_data_small_list) == perimeters
+# def test_get_areas_med():
+#     areas = {"O": 21, "X": 4}
+#     assert get_areas(test_data_med_list) == areas
 
 
-def test_get_perimeters_med():
-    perimeters = {"O": 36, "X": 16}
-    assert get_perimeters(test_data_med_list) == perimeters
+# def test_get_perimeters_small():
+#     perimeters = {"A": 10, "B": 8, "C": 10, "D": 4, "E": 8}
+#     assert get_perimeters(test_data_small_list) == perimeters
 
 
-def test_small():
-    assert run(TEST_DATA_SMALL) == 140
+# def test_get_perimeters_med():
+#     perimeters = {"O": 36, "X": 16}
+#     assert get_perimeters(test_data_med_list) == perimeters
 
 
-def test_medium():
-    assert run(TEST_DATA_MED) == 772
+# def test_small():
+#     assert run(TEST_DATA_SMALL) == 140
+
+
+# def test_medium():
+#     assert run(TEST_DATA_MED) == 772
 
 
 # def test_large():
