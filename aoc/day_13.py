322375b2182aae62d8c4b74ca4abc84f3961dbf7
Joe Boyle <jboyle617@gmail.com>
2025-01-27 23:45:04 -0500

    day 13 part 1 wip

--- aoc/day_13.py
+++ aoc/day_13.py
@@ -1,3 +1,5 @@
+import re
+
 DATA_FILE = "day_13.txt"
 
 TEST_DATA = """
@@ -18,6 +20,33 @@ Button B: X+27, Y+71
 Prize: X=18641, Y=10279
 """
 
+type ButtonCoords = tuple[int, int]
+type PrizeCoords = tuple[int, int]
+
+
+def parse_machine(
+    machine: str,
+) -> tuple[ButtonCoords, ButtonCoords, PrizeCoords]:
+    a, b, prize = machine.splitlines()
+
+    a_match = re.match(r"Button A: X\+(\d+), Y\+(\d+)", a)
+    b_match = re.match(r"Button B: X\+(\d+), Y\+(\d+)", b)
+    prize_match = re.match(r"Prize: X=(\d+), Y=(\d+)", prize)
+
+    if a_match is None or b_match is None or prize_match is None:
+        raise RuntimeError("bonk")
+
+    (ax, ay) = a_match.group(1), a_match.group(2)
+    (bx, by) = b_match.group(1), b_match.group(2)
+    (prize_x, prize_y) = prize_match.group(1), prize_match.group(2)
+
+    return (int(ax), int(ay)), (int(bx), int(by)), (int(prize_x), int(prize_y))
+
 
 def run(data: str, part_2: bool):
-    print(data)
+    """
+    You wonder: what is the smallest number of tokens you would have to spend to win as many prizes as possible?
+    """
+    machines = data.strip().split("\n\n")
+    parsed = list(map(parse_machine, machines))
+    return parsed
