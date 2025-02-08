b8419b571c28a11ac0d501cd387e8e456c6c5e44
Joe Boyle <jboyle617@gmail.com>
2025-02-08 08:26:35 -0500

    day 15 part 1 wip

---
+++ aoc/day_15.py
@@ -0,0 +1,68 @@
+DATA_FILE = "day_15.txt"
+
+test_data_small = """
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<
+"""
+
+TEST_DATA = """
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+"""
+
+Dir = {
+    "<": (-1, 0),
+    ">": (1, 0),
+    "^": (0, -1),
+    "v": (0, 1),
+}
+
+
+def walk(room: list[list[str]], moves: str):
+    start = (0, 0)
+    for y, row in enumerate(room):
+        for x, sq in enumerate(row):
+            if sq == "@":
+                start = (x, y)
+
+    for move in moves:
+        next_x, next_y = Dir[move][0], Dir[move][1]
+        if room[next_y][next_x] == "#":
+            continue
+
+
+def parse_input(data: str) -> tuple[list[list[str]], str]:
+    room_input, moves = data.split("\n\n")
+    room = [list(row) for row in room_input]
+    return room, moves
+
+
+def run(data: str, part_2: bool):
+    return parse_input(data)
